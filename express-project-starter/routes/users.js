fd0b047a52a4e9b462966e26905db6079b5f3e24
daniel <danielthai14@gmail.com>
2022-01-18 15:22:26 -0700

    user login and signup form working

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const { loginUser, logoutUser } = require('../auth.js')
 
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+
+const router = express.Router();
+
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('user-login', {
+    title: 'Login',
+    csrfToken: req.csrfToken(),
+  })
+});
+
+router.get('/signup', csrfProtection, (req, res) => {
+  res.render('signup-form', {
+    title: 'Sign-up',
+    csrfToken: req.csrfToken(),
+  })
 });
 
 module.exports = router;
