9e6b39b5f853ecf030a54a1ab86dda5b8f2a3a10
daniel <danielthai14@gmail.com>
2022-01-18 14:46:19 -0700

    user login route

---
+++ express-project-starter/db/seeders/20220118212601-Users_seeds.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Users', [
+     { id: 1, username: 'demo_user', email: 'demo@demo.com', hashedPassword: 'Passw0rd!', createdAt: new Date(), updatedAt: new Date() }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Users', null, {});
+  }
+};
