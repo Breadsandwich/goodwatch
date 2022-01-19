919faabc073acbea07f500b03ea881b1180e78ea
daniel <danielthai14@gmail.com>
2022-01-19 11:01:53 -0700

    added shows and watchlist migrations/seeder template

---
+++ express-project-starter/db/migrations/20220119174906-create-watchlist.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Watchlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+        unique: true
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        refrences: { model: 'Users' }
+      },
+      showId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        refrences: { model: 'Shows' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Watchlists');
+  }
+};
---
+++ express-project-starter/db/seeders/20220119174147-shows_seeds.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Shows', [
+     {id: 1, name: 'John Doe', description: 'some text', overallRating:'5.00', watchStatus: 'wants to watch', genre: 'example fantasy', createdAt: new Date(), updatedAt: new Date() },
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Shows', null, {});
+  }
+};
