96f29cec03573f22c2bbe9913760ecd10b315603
wilvanson <wilvanson.dutervil@students.cookman.edu>
2022-01-18 19:31:04 -0500

    login-user

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -9,6 +9,55 @@ const { User } = db;
 
 const router = express.Router();
 
+const userVal = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide an User Name.')
+    .isLength({ max: 50 })
+    .withMessage('Email cannot be longer than 50 charachters.'),
+  check('email')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an email.')
+        .isLength({ max: 255 })
+        .withMessage('Email cannot be longer than 255 charachters.')
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .custom(async (email) => {
+            const user = await User.findOne({ where: { email } })
+            if (user) throw new Error('Email already in use.')
+        }),
+  check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Password')
+        .isLength({ max: 50 })
+        .withMessage('Password must not be more than 50 characters long')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+        .withMessage('Password must contain atleast 1 lowercase letter, uppercase letter, number, and special charachter (i.e "!@#$%^&*") '),
+  check('confirmPassword')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Confirm Password')
+        .isLength({ max: 50 })
+        .withMessage('Confirm Password must not be more than 50 characters long')
+        .custom((confirmPassword, { req }) => {
+            if (confirmPassword !== req.body.password) {
+                throw new Error('Confirm Password does not match Password');
+            }
+            return true;
+        })
+
+
+];
+
+const loginValidators = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
+
 /* GET users listing. */
 router.get('/login', csrfProtection, (req, res) => {
   res.render('user-login', {
@@ -17,19 +66,26 @@ router.get('/login', csrfProtection, (req, res) => {
   })
 });
 
-router.post('/login', csrfProtection, asyncHandler(async(req, res) => {
-  const {username, email, hashPassword} = req.body;
-  const password = await bcrypt(hashPassword, 10);
-  const user = await User.build({
-    username,
-    email,
-    hashPassword = password
-  })
-  res.render('user-login', {
-    title: 'Login',
-    csrfToken: req.csrfToken(),
-  })
-}));
+router.post('/login', csrfProtection, loginValidators, asyncHandler(async(req,res)=>{
+  const {email, password} = req.body;
+  const validatorError = validationResult(req);
+  console.log(validatorError)
+  // console.log(user);
+  if(validatorError.isEmpty()){
+    const user = await User.findOne({
+      where: {email}
+    });
+    if(user){
+      const match = await bcrypt.compare(password, user.hashedPassword.toString());
+      if(match){
+        loginUser(req,res,user)
+        return res.redirect('/');
+      }
+    }
+  }
+}))
+
+
 router.get('/signup', csrfProtection, (req, res) => {
   res.render('signup-form', {
     title: 'Sign-up',
@@ -37,4 +93,32 @@ router.get('/signup', csrfProtection, (req, res) => {
   })
 });
 
+router.post('/signup', csrfProtection, userVal, asyncHandler(async(req, res) => {
+  const {username, email, password} = req.body;
+
+  console.log('----------- Hello');
+  const user = await User.build({
+    username,
+    email,
+    
+  });
+  const validatorError = validationResult(req);
+  if(validatorError.isEmpty()){
+    const hashPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashPassword;
+    await user.save();
+    loginUser(req,res,user);
+    return res.redirect('/');
+  }else{
+    const errors = validatorError.array().map((error) => error.msg);
+            res.render('signup-form', {
+                title: 'Signup',
+                user,
+                errors,
+                csrfToken: req.csrfToken(),
+            });
+
+  }
+  
+}));
 module.exports = router;
