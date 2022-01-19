d86ced17eb1fd3f416866d0b509ee610e8983c0b
daniel <danielthai14@gmail.com>
2022-01-19 15:59:45 -0700

    fixed config/database

--- config/database.js
+++ config/database.js
@@ -3,21 +3,22 @@ const {
 } = require('./index');
 
 module.exports = {
-  development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: 'postgres',
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    dialectOptions: { // << THIS OBJECT
-      ssl: { // << WITH THIS NESTED OBJECT
-        require: true, // <<vv AND THESE KEY:VALUE PAIRS
-        rejectUnauthorized: false,
-      },
+    development: {
+      username,
+      password,
+      database,
+      host,
+      dialect: 'postgres',
     },
-},
+    production: {
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres',
+      seederStorage: 'sequelize',
+      dialectOptions: { // << THIS OBJECT
+        ssl: { // << WITH THIS NESTED OBJECT
+          require: true, // <<vv AND THESE KEY:VALUE PAIRS
+          rejectUnauthorized: false,
+        },
+      },
+  }
+}
