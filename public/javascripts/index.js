115f3dc627986704132ba0c3bebbcdc3b6986763
Charles Woods <charles.woods48@yahoo.com>
2022-01-21 17:47:15 -0600

    api route, not done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
+window.addEventListener("DOMContentLoaded", (event)=>{
+    const hasWatchedButton = document.getElementsByClassName('watch')
+    const wantsToWatchButton = document.getElementsByClassName('wantsToWatch')
+    const currentlyButton = document.getElementsByClassName('currentlyWatch')
+
+    
+
+    const hasWatchedCount = document.getElementsByClassName('hasWatchedCount')
+    const wantsToWatchCount = document.getElementsByClassName('wantsToWatchCount')
+    const currentlyCount = document.getElementsByClassName('currentlyCount')
+
+    hasWatchedButton.addEventListener('click', () =>{
+        
+    })
 })
